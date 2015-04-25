a9c21b5f58a67a5b84c6d5b438fe8fa2d3702b8a
russellhorton <russ.horton@gmail.com>
2015-04-25 14:23:57 +0200

    missed some files...

---
+++ EpilepsySite/EpilepsySite.Web/Models/SyncDataModel.cs
@@ -0,0 +1,19 @@
+﻿using EpilepsySite.Web.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EpilepsySite.Web.Models
+{
+    public class SyncDataModel
+    {
+        public DateTime DateTime { get; set; }
+        public int UserId { get; set; }
+        public float Lat { get; set; }
+        public float Long { get; set; }
+        public float Alt { get; set; }
+        public float Accuracy { get; set; }
+        public List<HeartRateItem> HeartRatePackets { get; set; }
+    }
+}
---
+++ EpilepsySite/EpilepsySite.Web/Objects/HeartRateItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EpilepsySite.Web.Objects
+{
+    public class HeartRateItem
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int HeartRate { get; set; }
+        public DateTime DateTime { get; set; }
+        public int SyncId { get; set; }
+    }
+}
