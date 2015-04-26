657fe64bd771a10dc7d9f533b68c2237076b9fd2
russellhorton <russ.horton@gmail.com>
2015-04-26 09:09:52 +0200

    adding missing files

---
+++ EpilepsySite/EpilepsySite.Web/Controllers/ReportController.cs
@@ -0,0 +1,34 @@
+﻿using EpilepsySite.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace EpilepsySite.Web.Controllers
+{
+    public class ReportController: SurfaceController
+    {
+        public ActionResult GetAllData()
+        {
+
+            ReportModel model = new ReportModel();
+
+            if (!string.IsNullOrEmpty(Request.QueryString["userid"]))
+            {
+                int userid = int.Parse(Request.QueryString["userid"]);
+                model.heartRateItems = Data.HeartRate.GetAllHeartRateItemsByUserId(userid);
+                model.motionSensorItems = Data.MotionSensor.GetAllMotionSensorItemsByUserId(userid);
+            }
+            else 
+            { 
+                model.heartRateItems = Data.HeartRate.GetAllHeartRateItems();
+                model.motionSensorItems = Data.MotionSensor.GetAllMotionSensorItems();
+            }
+
+            return PartialView("Reports/ReportTable", model);
+
+        }
+    }
+}
---
+++ EpilepsySite/EpilepsySite.Web/Helpers/EmailHelper.cs
@@ -0,0 +1,56 @@
+﻿using SendGrid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace EpilepsySite.Web.Helpers
+{
+    public class EmailHelper
+    {
+        public static void SendEmail(string toAddress, string fromAddress, string subject, string messageBody)
+        {
+            // Create the email object first, then add the properties.
+            var myMessage = new SendGridMessage();
+
+            // Add the message properties.
+            myMessage.From = new MailAddress(fromAddress);
+
+            // Add multiple addresses to the To field.
+            List<String> recipients = new List<String>
+            {
+               toAddress                
+            };
+
+            myMessage.AddTo(recipients);
+
+            myMessage.Subject = subject;
+
+            //Add the HTML and Text bodies
+            myMessage.Html = messageBody;
+            myMessage.Text = messageBody;
+
+            // Create network credentials to access your SendGrid account
+            var username = Configuration.ConfigurationManager.SendGridUserName;
+            var pswd = Configuration.ConfigurationManager.SendGridPassword;
+
+            /* Alternatively, you may store these credentials via your Azure portal
+               by clicking CONFIGURE and adding the key/value pairs under "app settings".
+               Then, you may access them as follows: 
+               var username = System.Environment.GetEnvironmentVariable("SENDGRID_USER"); 
+               var pswd = System.Environment.GetEnvironmentVariable("SENDGRID_PASS");
+               assuming you named your keys SENDGRID_USER and SENDGRID_PASS */
+            
+            var credentials = new NetworkCredential(username, pswd);
+
+            // Create an Web transport for sending email.
+            var transportWeb = new SendGrid.Web(credentials);
+            
+            // Send the email.
+            // You can also use the **DeliverAsync** method, which returns an awaitable task.
+            transportWeb.DeliverAsync(myMessage);
+        }
+    }
+}
